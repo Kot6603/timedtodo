b7d32998f2d50ea3302f5699c07864eb530ee822
Kot <89110272+Kot6603@users.noreply.github.com>
2024-01-20 19:02:31 +1300

    Finished the functionality for the todo list

---
+++ src/TodoItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { TodoProps } from './TodoList';
+
+function TodoItem({ todo, deleteTodo, toggleCompleted }: { todo: TodoProps, deleteTodo: Function, toggleCompleted: Function }): React.ReactElement {
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      <p className={todo.completed ? 'completed' : ''}>{todo.text}</p>
+      <button onClick={() => deleteTodo(todo.id)} >X</button>
+    </div>
+  )
+}
+
+export default TodoItem
---
+++ src/TodoList.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import TodoItem from './TodoItem'
+
+export type TodoProps = {
+  id: number,
+  text: string,
+  completed: boolean,
+}
+
+
+function TodoList(): React.ReactElement {
+  const [todos, setTodos] = useState<TodoProps[]>([])
+  const [text, setText] = useState<string>('')
+
+  function addTodo(text: string): void {
+    const newTask: TodoProps = {
+      id: Date.now(),
+      text,
+      completed: false,
+    }
+
+    setTodos([...todos, newTask])
+    setText('')
+  }
+
+  function deleteTodo(id: number): void {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+
+  function toggleCompleted(id: number): void {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      }
+      return todo
+    }));
+  }
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+
+      <form onSubmit={e => {
+        e.preventDefault()
+        addTodo(text)
+      }}>
+        <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        <button type="submit">Add Task</button>
+      </form>
+
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleCompleted={toggleCompleted}
+        />
+      ))}
+
+    </div >
+  )
+}
+
+export default TodoList;
